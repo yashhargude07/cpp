c08c83fd591eb1f758144bb48d6e15bccc10682f
Hargude Yash Santosh <156149700+yashhargude07@users.noreply.github.com>
2024-09-20 22:32:42 +0530

    Add files via upload

---
+++ global_and_local_variable.cpp
@@ -0,0 +1,19 @@
+//global and local variable Declaration..
+
+#include<iostream>
+using namespace std;
+//global variable declartion:
+int g;
+int main()
+{
+    //Local variable declration:
+    int a,b;
+
+    //actual iniatization
+    a=10;
+    b=20;
+    g =a+b; 
+    cout<< g;
+    return 0;
+
+}
