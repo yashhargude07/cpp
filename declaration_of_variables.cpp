9dc617b985cddbe228b2d731f390fbe41b12f86e
Hargude Yash Santosh <156149700+yashhargude07@users.noreply.github.com>
2024-09-20 22:31:14 +0530

    Add files via upload

---
+++ declaration_of_variables.cpp
@@ -0,0 +1,23 @@
+//program for declaration of variabless..
+
+#include<iostream>
+using namespace std;
+int main()
+{
+
+//declaring variables
+int a, b;
+int result;
+
+//initialization of variable
+a=5;
+b=2;
+a=a+1;
+result = a-b;
+
+//print out the result
+cout<<result;
+
+//terminate the program
+return 0;
+}
